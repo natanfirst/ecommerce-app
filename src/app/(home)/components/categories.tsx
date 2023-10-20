c7b07b288186487d8f746959182858943d9aeede
natan alves <natanaelalvesnot@gmail.com>
2023-10-20 12:25:46 -0300

    feat: add categories and category item

---
+++ src/app/(home)/components/categories.tsx
@@ -0,0 +1,52 @@
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { Category } from "@prisma/client";
+import {
+  HeadphonesIcon,
+  KeyboardIcon,
+  MonitorIcon,
+  MouseIcon,
+  SpeakerIcon,
+  SquareIcon,
+} from "lucide-react";
+
+interface CategoryItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategoryItemProps) => {
+  const categoryIcon = {
+    keyboards: <KeyboardIcon size={16} />,
+    monitors: <MonitorIcon size={16} />,
+    headphones: <HeadphonesIcon size={16} />,
+    mousepads: <SquareIcon size={16} />,
+    speakers: <SpeakerIcon size={16} />,
+    mouses: <MouseIcon size={16} />,
+  };
+
+  return (
+    <Badge
+      className="flex items-center justify-center gap-3 rounded-lg px-3 py-3"
+      variant="outline"
+    >
+      <div className="flex min-w-[100px] items-center justify-start gap-x-3">
+        {categoryIcon[category.slug as keyof typeof categoryIcon]}
+        <span className="text-xs font-bold">{category.name}</span>
+      </div>
+    </Badge>
+  );
+};
+
+const Categories = async () => {
+  const categories = await prismaClient.category.findMany({});
+
+  return (
+    <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import Image from "next/image";
+import Categories from "./components/categories";
 
 export default function Home() {
   return (
     <div className="p-5">
-      <Image className="h-auto w-full" sizes="100vw" src="/banner-home-01.png" alt="Até 55% de desconto esse mês" width={350} height={150} />
+      <Image
+        className="h-auto w-full"
+        sizes="100vw"
+        src="/banner-home-01.png"
+        alt="Até 55% de desconto esse mês"
+        width={350}
+        height={150}
+      />
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   );
 }
