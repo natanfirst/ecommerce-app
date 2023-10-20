6e382f42d9524e93412c1bedd4c62e1a4a0b5286
natan alves <natanaelalvesnot@gmail.com>
2023-10-20 11:41:01 -0300

    feat: add banner to homepage

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image className="h-auto w-full" sizes="100vw" src="/banner-home-01.png" alt="Até 55% de desconto esse mês" width={350} height={150} />
+    </div>
+  );
+}
